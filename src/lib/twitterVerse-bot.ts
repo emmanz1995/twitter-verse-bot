97cfd63ee2842a5bbecd44b251be0498666e705f
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2023-03-28 00:30:06 +0100

    tried to fix the issues with the twitter api packages

--- src/lib/twitterVerse-bot.ts
+++ src/lib/twitterVerse-bot.ts
@@ -2,30 +2,26 @@ import twit from 'twit';
 import config from '../config';
 // import { generateRandomBibleVerse } from '../helpers/generateRandomBibleVerse';
 // import { scriptures } from '../connectors/data';
+import { TwitterApi } from 'twitter-api-v2';
 
-const twitter = new twit({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token: config.access_token,
-  access_token_secret: config.access_token_secret,
+const twitter = new TwitterApi({
+  appKey: config.consumer_key,
+  appSecret: config.consumer_secret,
+  accessToken: config.access_token,
+  accessSecret: config.access_token_secret,
 });
 
-function getAllTweets() {}
-function postNewTweets() {
-  twitter.post(
-    '2/tweets',
-    {
-      status:
-        'In the beginning was the word, and the word was with God, and the word was God.',
-    },
-    (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(data);
-      }
-    }
+const bearerToken = new TwitterApi(`${config.bearer_token}`);
+
+const client = twitter.readWrite;
+const readOnlyClient = bearerToken.readOnly;
+
+// function getAllTweets() {}
+async function postNewTweets() {
+  const tweet = await client.v2.tweet(
+    'In the beginning was the word, and the word was with God, and the word was God.'
   );
+  console.log(tweet);
 }
 
-export { postNewTweets, getAllTweets };
+export { postNewTweets };
