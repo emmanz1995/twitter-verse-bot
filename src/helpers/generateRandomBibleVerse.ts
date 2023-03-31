5ee9fe6ea98182ea88b76163818f7bddb2b93076
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2023-04-01 00:09:44 +0100

    fixing the random verse helper function

--- src/helpers/generateRandomBibleVerse.ts
+++ src/helpers/generateRandomBibleVerse.ts
@@ -1,6 +1,20 @@
-import { scriptures } from '../connectors/data';
+import { scriptures, IScriptures } from '../connectors/data';
 
-export const generateRandomBibleVerse = (scripture: string[]) => {
-  const bibleVerse = Math.floor(Math.random() * scripture.length);
-  return scriptures[bibleVerse];
+export const generateRandomBibleVerse = (scripture: IScriptures[]) => {
+  // const bibleVerse = Math.floor(Math.random() * scripture.length);
+  // return scriptures[bibleVerse];
+  let bibleVerse = [];
+
+  const randomNumber = Math.floor(Math.random() * 50 + 1);
+
+  if (randomNumber < 25) {
+    let randomIndex = Math.floor(Math.random() * scripture.length);
+    bibleVerse.push(scripture[randomIndex]);
+  }
+
+  if (typeof bibleVerse === null) {
+    return scriptures[0];
+  }
+
+  return bibleVerse;
 };
