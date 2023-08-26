f818507a2c43b6206aa0bb1a946e5fa6afb69c27
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2023-08-26 11:16:56 +0100

    new changes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,1 @@
-import express, { Express } from 'express';
-import configureCors from './config/cors';
-import { testController } from './config/adapter';
-import MongoDB from './config/mongo';
 
-const app: Express = express();
-const PORT: number = 5001;
-
-// Rest of your server configuration and routes
-configureCors(app);
-testController(app);
-
-MongoDB.getInstance();
-
-app.listen(PORT, () => {
-  console.log(`Listening on PORT: ${PORT}`);
-});
